ae3ec46e4d7469bb1ded4173af20c4105e423e66
yu-gotcha <yu.gotcha@gmail.com>
2021-07-12 19:18:20 +0900

    add redis configuration

--- src/main/java/com/choulatte/scentproduct/ScentProductApplication.java
+++ src/main/java/com/choulatte/scentproduct/ScentProductApplication.java
@@ -2,8 +2,10 @@ package com.choulatte.scentproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCaching
 @EnableSwagger2
 @SpringBootApplication
 public class ScentProductApplication {
---
+++ src/main/java/com/choulatte/scentproduct/config/EmbeddedRedisConfig.java
@@ -0,0 +1,31 @@
+package com.choulatte.scentproduct.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void redisServer() {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+
+}
